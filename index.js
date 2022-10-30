07f3ef73016c2d90917fe467ef4ab8016eff8904
zigmons <rafael_sousa87@hotmail.com>
2022-10-29 23:52:04 -0300

    Fim

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ function btn(){
     let contribuicao = document.getElementById('contribuicao')
     let nome = document.getElementById("nome")
     let mensalidade1 = parseFloat(mensalidade.value)
+    let mensalidade2 = mensalidade.value
     let contribuicao1 = contribuicao.value
     let contribuicao2 = parseFloat(contribuicao1*12)
     let nome1 = nome.value
     let juros = 0.00517
-
-    let expr = parseFloat(mensalidade1 * (((1 + juros) ** [contribuicao2] - 1) / juros).toFixed(3))
-    // let total = (mensalidade1*(contribuicao1*12))
-    // let total_2 = (total + expr).toFixed(2)
-
-    let div = document.getElementById('div1')
-    let h4 = div.innerHTML = `<h4>Olá ${nome1}, juntando R$ ${mensalidade1} todo mês, você terá R$ ${expr.toFixed(2)} em ${contribuicao1} anos</h4>
-    <button class="button" type="button" onclick="voltar()">Simular Novamente!</button>`
     
+    if (nome1 == "" || typeof nome1 == "number") { 
+        let div = document.getElementById('infos')
+        let h4 = div.innerHTML = `<h4>Por favor, preencha seu nome!</h4>`
+    }else if(mensalidade2 == "" || contribuicao1 ==""){
+        let div = document.getElementById('infos')
+        let h4 = div.innerHTML = `<h4>Por favor, preencha os campos de mensalidade e tempo de contribuição!</h4>`
+    }else{
+        let expr = parseFloat(mensalidade1 * (((1 + juros) ** [contribuicao2] - 1) / juros).toFixed(3))
+        let div = document.getElementById('div1')
+        let h4 = div.innerHTML = `<h4>Olá ${nome1}, juntando R$ ${mensalidade1} todo mês, você terá R$ ${expr.toFixed(2)} em ${contribuicao1} anos</h4>
+        <button class="button" type="button" onclick="voltar()">Simular Novamente!</button>`
+    }
 }
-// 
 
 function voltar(){
     let div = document.getElementById('body')
-    let h4 = div.innerHTML = `     <body id="body">
+    let h4 = div.innerHTML = `    <body id="body">
     <main>
         <header class="header">
             <img src="./Imagens/porquinho.png"
@@ -30,27 +34,26 @@ function voltar(){
         </header>
         <div id="div1">
             <form>
+                
                 <div>
                     <label class="label" for="">Nome:</label>
                     <input type="text" id="nome" class="input" placeholder="Digite seu nome">
-                    <!-- <br></br> -->
                 </div>
-                <div>
+                <div id="mensalidade_campo">
                     <label class="label" for="">Mensalidade:</label>
                     <input type="number" id="mensalidade" class="input" placeholder="Digite o valor mensal">
-                    <!-- <br></br> -->
                 </div>
                 <div>
                     <label class="label" for="">Tempo de Contribuição (Anos):</label>
                     <input type="number" id="contribuicao" class="input" placeholder="Digite o periodo">
-                    <!-- <br></br> -->
                 </div>
                 <!-- <input type="button" id="send" onclick="btn()"
                 alue="Enviar"> -->
+                <div id='infos'></div>
                 <button class="button" type="button" onclick="btn()">Simular</button>
             </form>
         </div>
-        <div id='infos'></div>
+
         <footer>
             <p> Desafio JS- Simulador</p>
         </footer>
